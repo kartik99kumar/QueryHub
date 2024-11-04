93b0de3d5a98f143ec2b9604b7d9db6fc62dd526
kartik99kumar <kartik99kumar@gmail.com>
2024-11-04 16:28:49 +0530

    handled the case where there are no trending searches

--- frontend/src/components/TrendingSearches.js
+++ frontend/src/components/TrendingSearches.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { ListGroup } from "react-bootstrap";
+import { Row, Col, ListGroup } from "react-bootstrap";
 import axios from "axios";
 
 function TrendingSearches({ onSelect }) {
@@ -9,25 +9,30 @@ function TrendingSearches({ onSelect }) {
     axios
       .get("/trending")
       .then((response) => {
-        setTrending(response.data);
+        setTrending(response.data || []); // Ensure trending is an array
       })
       .catch((error) => {
         console.error("Unable to retrieve trending searches", error);
+        setTrending([]); // Fallback to an empty array on error
       });
   }, []);
 
   return (
-    <div>
-      <ListGroup className='mt-4'>
-        {trending.slice(0, 5).map((item, index) => (
-          <ListGroup.Item
-            key={index}
-            action
-            onClick={() => onSelect(item.query)}
-            className='trending-item  d-flex justify-content-center align-items-center text-center'>
-            {item.query}
-          </ListGroup.Item>
-        ))}
+    <div className='mt-4'>
+      <ListGroup>
+        <Row>
+          {(Array.isArray(trending) ? trending.slice(0, 4) : []).map(
+            (item, index) => (
+              <ListGroup.Item
+                key={index}
+                action
+                onClick={() => onSelect(item.query)}
+                className='trending-item d-flex justify-content-center align-items-center text-center'>
+                {item.query}
+              </ListGroup.Item>
+            )
+          )}
+        </Row>
       </ListGroup>
     </div>
   );
