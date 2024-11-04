f8e20040239ff9daf59abad719aa391522a51854
kartik99kumar <kartik99kumar@gmail.com>
2024-11-04 19:55:43 +0530

    changed the trending endpoint to also provide cached response if available

--- backend/controllers/trendingController.js
+++ backend/controllers/trendingController.js
@@ -10,12 +10,16 @@ const getTrendingQueries = asyncHandler(async (req, res) => {
       4,
       { REV: true }
     );
-
-    const result = trendingQueries.map((entry) => ({
-      query: entry.value,
-      count: entry.score,
-    }));
-
+    const result = await Promise.all(
+      trendingQueries.map(async (entry) => {
+        const cachedResponse = await redisClient.get(entry.value);
+        return {
+          query: entry.value,
+          count: entry.score,
+          response: cachedResponse ? JSON.parse(cachedResponse) : null,
+        };
+      })
+    );
     res.json(result);
   } catch (error) {
     res.status(500).json({ error: "Failed to fetch trending queries." });
